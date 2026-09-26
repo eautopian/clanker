a9c59be945b7ab68cbbc685bca7740ca7c6d4fce
Pxsl <151259803+pxsldev@users.noreply.github.com>
2026-09-26 13:28:05 +0100

    updatred theming

--- cogs/theming.py
+++ cogs/theming.py
@@ -8,30 +8,39 @@ THEMES = {
         "fail": discord.Color.purple(),
         "avatar": "assets/halloween/avatar.png",
         "banner": "assets/halloween/banner.png",
+        "name": "Spooky Clanker 🎃",
     },
+
     "fools": {
         "success": discord.Color.yellow(),
         "fail": discord.Color.magenta(),
         "avatar": "assets/fools/avatar.png",
         "banner": "assets/fools/banner.png",
+        "name": "clanker uwu :3",
     },
+
     "christmas": {
         "success": discord.Color.green(),
         "fail": discord.Color.red(),
         "avatar": "assets/christmas/avatar.png",
         "banner": "assets/christmas/banner.png",
+        "name": "Merry Clanker🎄",
     },
+
     "easter": {
         "success": discord.Color.green(),
         "fail": discord.Color.pink(),
         "avatar": "assets/easter/avatar.png",
         "banner": "assets/easter/banner.png",
+        "name": "Clanker Bunny 🐰",
     },
+
     "default": {
         "success": discord.Color.blurple(),
         "fail": discord.Color.red(),
         "avatar": "assets/default/avatar.png",
         "banner": "assets/default/banner.png",
+        "name": "Clanker",
     },
 }
 
@@ -63,6 +72,9 @@ def get_avatar():
 def get_banner():
     return THEMES[cached_theme]["banner"]
 
+def get_name():
+    return THEMES[cached_theme]["name"]
+
 class Theming(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -75,26 +87,38 @@ class Theming(commands.Cog):
     async def apply_theme(self):
         now = datetime.datetime.now(datetime.timezone.utc)
         date = now.date()
+
         if self.current_date == date:
             return
 
         global cached_theme
         cached_theme = get_theme()
-
         self.current_date = date
 
         avatar_file = get_avatar()
         banner_file = get_banner()
+        name = get_name()
 
         with open(avatar_file, "rb") as f:
             avatar = f.read()
 
         with open(banner_file, "rb") as f:
             banner = f.read()
-    
-        await self.bot.user.edit(avatar=avatar, banner=banner)
 
-    @tasks.loop(time=datetime.time(hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc))
+        await self.bot.user.edit(
+            username=name,
+            avatar=avatar,
+            banner=banner
+        )
+
+    @tasks.loop(
+        time=datetime.time(
+            hour=0,
+            minute=0,
+            second=0,
+            tzinfo=datetime.timezone.utc
+        )
+    )
     async def update_theme(self):
         await self.apply_theme()
 
